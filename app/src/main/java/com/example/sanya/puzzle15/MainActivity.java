505706518c67206a53097070946cd90ca2c65cc3
sanyoca <sanyoca@gmail.com>
2017-03-07 16:26:37 +0100

    Version 0.20
    - initiated a picture, a hole and a wall version of the puzzle, with a game selection screen. No Intent working currently.

--- app/src/main/java/com/example/sanya/puzzle15/MainActivity.java
+++ app/src/main/java/com/example/sanya/puzzle15/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.sanya.puzzle15;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
         TextView gameTitle = (TextView) findViewById(R.id.textview_gametitle);
         gameTitle.setTypeface(fontHarrington);
 
+        findViewById(R.id.button_startclassical).setOnClickListener(this);
+    }
+
+    public void OnClick (View view) {
 
     }
 }
